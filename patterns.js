e6b0b8efed98c3ea4fb2f2884bb14e5b8266d6ec
etikala archana <archanaetikala51@gmail.com>
2026-06-24 20:43:58 +0530

    initial commit

---
+++ patterns.js
@@ -0,0 +1,25 @@
+let n=4;
+for(let i=1;i<=n;i++){
+   let row="";
+   
+    for(let j=1;j<=i;j++)
+    {
+        row+="*";
+    }
+   console.log(row);
+}
+
+
+
+
+
+let m=5;
+for(let i=2;i<=m;i++){
+    let row=" ";
+    for(let j=1;j<=i;j++)
+    {
+        row+="*";
+    
+    }
+   console.log(row);
+}
