f31524bcb342ba284534e7f1dcb8808e4eb5cc06
etikala archana <archanaetikala51@gmail.com>
2026-07-02 14:34:02 +0530

    hoisting

--- functions/hoisting.js
+++ functions/hoisting.js
@@ -1,3 +1,19 @@
-
+var a;
 console.log(a);
-let a = 100;
+a = 100;
+console.log(a);
+
+
+// let keyword will not provide value
+let b;
+console.log(b);
+b = 100;
+console.log(b);
+
+
+greet();
+function greet()
+{
+    console.log("hello");
+
+}
